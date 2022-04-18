5ecfd6a7e5437fb0c8e8d75ca48519152743223a
hossainarif37 <hossainarif4937@gmail.com>
2022-04-18 22:12:13 +0600

    about me page updated

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <h1>This is about</h1>
+        <div className=' w-50 m-auto shadow-lg p-5' style={{ height: '500px' }}>
+            <h1 className='text-center'>Hello, I am Ariful Islam</h1>
+            <p>My aim is to be Google Engineer. I know this will be a though game for me. But i will never bow. I will chase for my dream. I will word hard and i will show the world that anyone who can desire something it can be achieved.</p>
         </div>
     );
 };
--- src/Components/Checkout/Checkout.js
+++ src/Components/Checkout/Checkout.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Checkout = () => {
     return (
-        <div>
+        <div className='text-center pt-5 text-success' style={{ height: '500px' }}>
             <h1>This is check out page</h1>
         </div>
     );
