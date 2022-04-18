6645db99777217b68d99ee28363ef1d47a1e6609
hossainarif37 <hossainarif4937@gmail.com>
2022-04-18 19:12:58 +0600

    footer section make responsive

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,11 +3,15 @@ import { Link } from 'react-router-dom';
 import './Footer.css'
 const Footer = () => {
     return (
-        <footer className='footer-section d-flex justify-content-center' style={{ backgroundColor: "#122033", width: "100%", padding: "30px 0" }}>
-            Copyright &copy; 2022,Visapress. All rights reserved.
-            <Link to='#'>Terms of Use</Link>
-            <span className='text-white'>|</span>
-            <Link to='#'>Privacy Policy</Link>
+        <footer className='footer-section d-flex flex-column flex-lg-row justify-content-center p-3 p-lg-5' style={{ backgroundColor: "#122033", width: "100%" }}>
+            <div>
+                Copyright &copy; 2022,Visapress. All rights reserved.
+            </div>
+            <div className='d-flex d-lg-block justify-content-center justify-content-lg-end'>
+                <Link to='#'>Terms of Use</Link>
+                <span className='text-white'>|</span>
+                <Link to='#'>Privacy Policy</Link>
+            </div>
         </footer>
     );
 };
